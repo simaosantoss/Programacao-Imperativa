d2427bd81d64e7c321991c38a200feede611b51f
Simão Pedro Ribeiro Santos <a109767@uminho.pt>
2025-06-08 12:49:21 +0100

    exercicio feito

--- Fichas/Ficha04/ficha4.c
+++ Fichas/Ficha04/ficha4.c
@@ -6,11 +6,15 @@
 
 // Exercicio 1.1
 
-int evogal (char c) {
+int evogal(char c)
+{
     char *v = "aAeEiIoOuU";
-    int i;
-    for (i = 0 ; v[i] != '\0' && v[i] != c ; i++);
-    return (v[i] != '\0');
+
+    for (int i = 0; v[i] != '\0'; i++)
+        if (v[i] == c)
+            return 1; // É vogal
+
+    return 0; // Não é vogal
 }
 
 int contavogais (char s[]) {
@@ -27,20 +31,61 @@ int contavogais (char s[]) {
 
 // Exercicio 1.2
 
-int retiraVogaisRep (char *s) {
-    int i , j , cont = 0;
-    i = 0;
-    j = 0;
+int retiraVogaisRep (char *s)
+{
+    int i = 0, j = 0, removidas = 0;
 
-    while (s[i] != '\0') {
-        if (!(evogal (s[i]) && s[i] == s[i+1])) {
-            s[j] = s[i]; j++; }
-        else cont++;
-        i++;
+    for (i = 0; s[i] != '\0'; i++)
+    {
+
+        if (evogal(s[i]) && s[i] == s[i+1])
+        {
+            while (s[i] == s[i+1])
+            {
+                i++;
+                removidas++;
+            }
+        }
+
+        s[j] = s[i];
+        j++;
     }
 
     s[j] = '\0';
-    return (s);
+    return removidas;
+}
+
+//Exercicio 1.3
+
+int duplicaVogais (char *s)
+{
+    int len = strlen(s);
+    int num_vogais = 0;
+
+    for (int z = 0; z < len ; z++)
+    {
+        if (evogal(s[z]))
+            num_vogais++;
+    }
+
+    int i = len - 1;              // Última posição do original
+    int j = len + num_vogais - 1; // Última posição do novo tamanho
+
+    while (i >= 0)
+    {
+        if (evogal(s[i]))
+        {
+            s[j--] = s[i];
+            s[j--] = s[i--];
+        }
+        else
+        {
+            s[j--] = s[i];
+        }
+        i--;
+    }
+
+    return num_vogais;
 }
 
 // Exercicio 2.1
