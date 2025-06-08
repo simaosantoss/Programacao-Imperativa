61faeae3f14a158f20777a773aa94f3c4355dd52
Simão Pedro Ribeiro Santos <a109767@uminho.pt>
2025-06-08 18:38:49 +0100

    ficha 6 refeita

--- Fichas/Ficha06/ficha6.c
+++ Fichas/Ficha06/ficha6.c
@@ -2,48 +2,124 @@
 
 /*--------------------------------------- Ficha 6 ----------------------------------------*/
 
-// Exercicio 2
-typedef struct staticQueue {
-int front;
-int length;
-int values [Max];
-} QUEUE, *SQueue;
+// Exercicio 1
+
+typedef struct staticStack {
+    int sp;
+    int values [Max];
+} STACK, *SStack;
 
 // a)
-void SinitQueue (SQueue q) {
-    q -> front = 0;
-    q -> length = 0;
+
+void SinitStack (SStack s)
+{
+    s->sp = 0;
 }
 
-// b)
-int SisEmptyQ (SQueue q) {
-    return (q -> length == 0);
+// b) 
+int SisEmpty (SStack s)
+{
+    return s->sp == 0;
 }
 
-// c) 
-int Senqueue (SQueue q, int x) {
-    int r = 0 , p;
-    if (q -> length == Max) r = 1;
-    else {
-        p = (q -> front + q -> length) % Max;
-        q -> values[p] = x;
-        q -> length ++;
+// c)
+int Spush (SStack s, int x)
+{
+    if (s->sp == Max) // sp já aponta para o máximo (stack cheia)
+            return 1;
+
+    else
+    {
+        s->values[s->sp] = x;
+        s->sp++; // Avança para a próxima posição
+        return 0;
     }
 }
 
-//d)
-int Sdequeue (SQueue q, int *x) {
-    if (q -> length == 0) return 1;
-    *x = q -> values[q -> front];
-    q -> length --;
-    q -> front ++;
-    if (q -> front == Max) q -> front = 0;
-    return 0;
+// d)
+int Spop (SStack s, int *x)
+{
+    if (s->sp == 0) //stack vazia
+        return 1;
+
+    else
+    {
+        s->sp--;               // recua o pointer, para apontar para o topo real
+        *x = s->values[s->sp]; // guarda o valor do topo (que está agora em s->sp)
+        return 0;
+    }
+}
+
+// e) 
+
+int Stop (SStack s, int *x)
+{
+    if (s->sp == 0)
+        return 1;
+
+    else
+    {
+        *x = s->values[s->sp - 1];
+        return 0;
+    }
+}
+
+// Exercicio 2
+typedef struct staticQueue {
+    int front;
+    int length;
+    int values [Max];
+} QUEUE, *SQueue;
+
+// a)
+void SinitQueue (SQueue q) 
+{
+    q->length = 0;
+    q->front = 0;
+}
+
+// b)
+int SisEmptyQ (SQueue q)
+{
+    return (q->length == 0);
+}
+
+// c)
+int Senqueue (SQueue q, int x)
+{
+    int p; 
+
+    if (q->length == Max)
+        return 1;
+
+    else
+    {
+        p = (q->front + q->length) % Max;
+        q->values[p] = x;
+        q->length++;
+        return 0;
+    }
+}
+
+// d) 
+int Sdequeue (SQueue q, int *x) 
+{
+    if (q->length == 0)
+        return 1;
+
+    *x = q->values[q->front];
+    q->front = (q->front + 1) % Max;
+    q->length--;
+
+    return 0; 
 }
 
 // e)
-int Sfront (SQueue q, int *x) {
-    if (q -> length == 0) return 1;
-    *x = q -> values[q -> front];
+int Sfront(SQueue q, int *x) 
+{
+    if (q->length == 0)
+        return 1;
+
+    *x = q->values[q->front];
     return 0;
 }
